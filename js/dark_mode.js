46fa864d4c2161ef35ffb92bb784edebe757ba28
Olga Matveeva <qbbyone8@mail.ru>
2021-09-21 14:01:47 +0300

    add loading, fix dark mode behavior

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,6 +1,35 @@
+// const darkModeCheckbox = document.getElementById("dark-mode-checkbox");
+
+// darkModeCheckbox.addEventListener("change", () => {
+//   // change the theme of the website
+//   document.body.classList.toggle("dark");
+// });
+
+
+let darkMode = localStorage.getItem("darkMode");
 const darkModeCheckbox = document.getElementById("dark-mode-checkbox");
 
+const enableDarkMode = () => {
+  document.body.classList.add("dark");
+  localStorage.setItem("darkMode", "enabled");
+  darkModeCheckbox.checked = true
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("dark");
+  localStorage.setItem("darkMode", null);
+  darkModeCheckbox.checked = false
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
 darkModeCheckbox.addEventListener("change", () => {
-  // change the theme of the website
-  document.body.classList.toggle("dark");
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 });
